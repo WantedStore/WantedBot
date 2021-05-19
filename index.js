6461a1b52bcdd427bb1d17aa7ff76a549c2d863b
ItaluuuS2 <fitalo238@gmail.com>
2021-05-18 22:58:38 -0300

    .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = require('quick.db');
 const prefix = process.env.prefix
 const token = process.env.token
 
-c
+const opa = require("opa")
 
 let seta = process.env.emoteseta
 let verificar = process.env.emoteverificar
